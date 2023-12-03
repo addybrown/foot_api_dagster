98dbf2463081a94a96f08c31e0815faa2986f441
Adam Sequeira <adam.sequeira@sportsiq.ai>
2023-12-02 20:03:48 -0500

    fix 23

--- assets.py
+++ assets.py
@@ -48,43 +48,64 @@ my_policy = AutoMaterializePolicy.eager().with_rules(
 )
 
 
-@asset(freshness_policy=FreshnessPolicy(maximum_lag_minutes=60))
+@asset(
+    freshness_policy=FreshnessPolicy(maximum_lag_minutes=60),
+    auto_materialize_policy=my_policy,
+)
 def update_schedule():
     update_schedule_table()
     return "Done"
 
 
-@asset(freshness_policy=FreshnessPolicy(maximum_lag_minutes=60))
+@asset(
+    freshness_policy=FreshnessPolicy(maximum_lag_minutes=60),
+    auto_materialize_policy=my_policy,
+)
 def update_match_details(update_schedule: str):
     if update_schedule == "Done":
         update_match_details_table()
 
 
-@asset(freshness_policy=FreshnessPolicy(maximum_lag_minutes=60))
+@asset(
+    freshness_policy=FreshnessPolicy(maximum_lag_minutes=60),
+    auto_materialize_policy=my_policy,
+)
 def update_match_odds(update_schedule: str):
     if update_schedule == "Done":
         update_match_odds_table()
 
 
-@asset(freshness_policy=FreshnessPolicy(maximum_lag_minutes=60))
+@asset(
+    freshness_policy=FreshnessPolicy(maximum_lag_minutes=60),
+    auto_materialize_policy=my_policy,
+)
 def update_match_shotmap(update_schedule: str):
     if update_schedule == "Done":
         update_match_shotmap_table()
 
 
-@asset(freshness_policy=FreshnessPolicy(maximum_lag_minutes=60))
+@asset(
+    freshness_policy=FreshnessPolicy(maximum_lag_minutes=60),
+    auto_materialize_policy=my_policy,
+)
 def update_match_incidents(update_schedule: str):
     if update_schedule == "Done":
         update_match_incidents_table()
 
 
-@asset(freshness_policy=FreshnessPolicy(maximum_lag_minutes=60))
+@asset(
+    freshness_policy=FreshnessPolicy(maximum_lag_minutes=60),
+    auto_materialize_policy=my_policy,
+)
 def update_match_lineup(update_schedule: str):
     if update_schedule == "Done":
         update_match_lineup_and_player_statistics()
 
 
-@asset(freshness_policy=FreshnessPolicy(maximum_lag_minutes=60))
+@asset(
+    freshness_policy=FreshnessPolicy(maximum_lag_minutes=60),
+    auto_materialize_policy=my_policy,
+)
 def update_match_statistics(update_schedule: str):
     if update_schedule == "Done":
         update_match_statistics_table()
